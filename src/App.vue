ea2a01f5acc8db120e166cf550df601223c84954
Evan Knapke <evanknapke@gmail.com>
2025-09-09 16:27:34 -0400

    Add toast system and replace old notifications

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-// Main app component
-</script>
-
 <template>
   <router-view />
+  <VToast :toasts="toasts" @remove-toast="toastStore.removeToast" />
 </template>
+
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import VToast from '@/components/ui/VToast.vue'
+import { useToastStore } from '@/stores/toastStore'
+
+const toastStore = useToastStore()
+const toasts = computed(() => toastStore.toasts)
+</script>
 
 <style scoped></style>
